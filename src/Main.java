e34b89cd481635650b39fb4a1e8a12d4ae6faecb
AlexNikiforenko <sashanikiforenko2004@gmail.com>
2022-09-14 12:06:42 +0300

    Practical Work #5 completed.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 import p4.Square;
+import p5.MovableCircle;
+import p5.MovablePoint;
 
 public class Main {
     public static void main(String[] args) {
-        Square square = new Square();
-        square.setSide(7);
-        System.out.println(square);
+
+//        MovablePoint point = new MovablePoint(4, 7, 2, 2);
+//        point.moveDown();
+//        point.moveRight();
+//        System.out.println(point);
+
+        MovableCircle circle = new MovableCircle(0, 0, 1, 1);
+        circle.moveUp();
+        circle.moveLeft();
+
+        System.out.println(circle);
     }
 }
---
+++ src/p5/MovablePoint.java
@@ -0,0 +1,37 @@
+package p5;
+
+public class MovablePoint implements Movable{
+    protected int x, y, xSpeed, ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public String toString() {
+        return "Point x = " + x + " Point y = " + y + '\n'
+                + "x speed = " + xSpeed + " y speed = " + ySpeed;
+    }
+
+    @Override
+    public void moveUp() {
+        y -= ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        y += ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        x += xSpeed;
+    }
+}
